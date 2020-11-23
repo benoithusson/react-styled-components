41f1581a933206cc5e64fd853996c14a66fe9468
Benoît <benoit.husson.tours@gmail.com>
2020-11-23 14:42:20 +0100

    Change Readme file + add project structure

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Nav = styled.nav`
+    background: #101522;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+`;
+
+const NavBar = () => {
+    return (
+        <>
+            <nav>
+
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// File where we export every components contain from components file
+
+export { default as NavBar } from './NavBar/NavBar';
+
